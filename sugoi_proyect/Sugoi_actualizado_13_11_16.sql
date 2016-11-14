3e3847e6c16304cb7848259285d92026c9089b13
Gerardo Rico <Rico@ASUS-U45J>
2016-11-13 22:51:08 -0600

    DATABASE

---
+++ sugoi_proyect/Sugoi_actualizado_13_11_16.sql
@@ -0,0 +1,424 @@
+CREATE DATABASE  IF NOT EXISTS `sugoi` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sugoi`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sugoi
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `empleado`
+--
+
+DROP TABLE IF EXISTS `empleado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `empleado` (
+  `id_empleado` int(10) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `ap_pat` varchar(20) DEFAULT NULL,
+  `ap_mat` varchar(20) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `direccion` varchar(250) DEFAULT NULL,
+  `mail` varchar(30) DEFAULT NULL,
+  `horas_trab` int(15) DEFAULT NULL,
+  `turno` varchar(20) DEFAULT NULL,
+  `antiguedad` int(15) DEFAULT NULL,
+  KEY `id_empleado` (`id_empleado`),
+  CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `login` (`id_empleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleado`
+--
+
+LOCK TABLES `empleado` WRITE;
+/*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
+INSERT INTO `empleado` VALUES (1,'Gerardo M','Rico','Carlos','55-29-36-85-18','Cda. Nezahualcoyotl, No 1,Chimalhuacan','ceo@easysoftmx.xyz',1,'Jefe',0);
+/*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jornadas`
+--
+
+DROP TABLE IF EXISTS `jornadas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jornadas` (
+  `clave_jornada` int(10) NOT NULL DEFAULT '0',
+  `id_empleado` int(10) DEFAULT NULL,
+  `hr_entrada` date DEFAULT NULL,
+  `hr_salida` date DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`clave_jornada`),
+  KEY `id_empleado` (`id_empleado`),
+  CONSTRAINT `jornadas_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `login` (`id_empleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jornadas`
+--
+
+LOCK TABLES `jornadas` WRITE;
+/*!40000 ALTER TABLE `jornadas` DISABLE KEYS */;
+INSERT INTO `jornadas` VALUES (1,1,'0000-00-00','0000-00-00','0000-00-00');
+/*!40000 ALTER TABLE `jornadas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login` (
+  `id_empleado` int(10) NOT NULL DEFAULT '0',
+  `usr` varchar(20) DEFAULT NULL,
+  `pwd` varchar(20) DEFAULT NULL,
+  `admin` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_empleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (1,'root','root','1');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pagos`
+--
+
+DROP TABLE IF EXISTS `pagos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pagos` (
+  `clave_pago` int(10) NOT NULL DEFAULT '0',
+  `hrs_trabajadas` int(20) DEFAULT NULL,
+  `incapacidad` varchar(5) DEFAULT NULL,
+  `total_pago` double DEFAULT NULL,
+  `id_empleado` int(10) NOT NULL,
+  PRIMARY KEY (`clave_pago`),
+  KEY `id_empleado_idx` (`id_empleado`),
+  CONSTRAINT `id_empleado` FOREIGN KEY (`id_empleado`) REFERENCES `login` (`id_empleado`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagos`
+--
+
+LOCK TABLES `pagos` WRITE;
+/*!40000 ALTER TABLE `pagos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'sugoi'
+--
+
+--
+-- Dumping routines for database 'sugoi'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `sp_alta_admon` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_alta_admon`(usr_in nvarchar(100),pwd_in nvarchar(100),nombre_in nvarchar(100),ap_in nvarchar(50),am_in nvarchar(50),tel_in nvarchar(20),dir_in nvarchar(200),mail_in nvarchar(50),horas_in nvarchar(2),turno_in nvarchar(200))
+begin
+declare existe int(2);
+declare id_usr_calc int(2);
+
+set existe = (select count(*) from login where usr = usr_in);
+
+if existe = 0 then
+
+set id_usr_calc = (select ifnull(max(id_empleado), 0) + 1 from login);
+
+INSERT INTO login VALUES (id_usr_calc,usr_in,pwd_in,1);
+INSERT INTO empleado VALUES(id_usr_calc,nombre_in,ap_in,am_in,tel_in,dir_in,mail_in,horas_in,turno_in,0);
+
+
+select concat('Registro exitoso, Su ID Unico es: ',id_usr_calc) as id_getted;
+else
+select "Ya existe el usuario" as id_getted;
+end if; 
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_alta_emp` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_alta_emp`(usr_in nvarchar(100),pwd_in nvarchar(100),nombre_in nvarchar(100),ap_in nvarchar(50),am_in nvarchar(50),tel_in nvarchar(20),dir_in nvarchar(200),mail_in nvarchar(50),horas_in nvarchar(2),turno_in nvarchar(200))
+begin
+declare existe int(2);
+declare id_usr_calc int(2);
+
+set existe = (select count(*) from login where usr = usr_in);
+
+if existe = 0 then
+
+set id_usr_calc = (select ifnull(max(id_empleado), 0) + 1 from login);
+
+INSERT INTO login VALUES (id_usr_calc,usr_in,pwd_in,0);
+INSERT INTO empleado VALUES(id_usr_calc,nombre_in,ap_in,am_in,tel_in,dir_in,mail_in,horas_in,turno_in,0);
+
+
+select concat('Registro exitoso, Su ID Unico es: ',id_usr_calc) as id_getted;
+else
+select "Ya existe el usuario" as id_getted;
+end if;
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_delete_user` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_delete_user`(usr_in nvarchar(50))
+begin
+
+declare id_usr_calc int(2);
+
+
+set id_usr_calc = (select id_empleado from login where usr = usr_in);
+
+DELETE FROM empleado WHERE id_empleado = id_usr_calc;
+DELETE FROM login WHERE id_empleado = id_usr_calc;
+
+select concat('Se ha eliminado con exito el usuario con id: ',id_usr_calc) as id_getted;
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_get_all_data_user` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_all_data_user`(usr_in nvarchar(50))
+begin
+
+declare id_usr_calc int(2);
+
+set id_usr_calc = (select id_empleado from login where usr = usr_in);
+select concat(nombre,' ',ap_pat,' ',ap_mat),tel,direccion,mail,horas_trab,turno,antiguedad from empleado where id_empleado = id_usr_calc;
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_get_all_data_user_modificacion` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_all_data_user_modificacion`(usr_in nvarchar(50))
+begin
+
+declare id_usr_calc int(2);
+
+set id_usr_calc = (select id_empleado from login where usr = usr_in);
+select nombre,ap_pat,ap_mat,tel,direccion,mail,horas_trab,turno,antiguedad from empleado where id_empleado = id_usr_calc;
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_get_all_users` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_all_users`()
+begin
+
+select usr from login;
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_get_report` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_report`()
+begin
+
+select concat(nombre,' ',ap_pat,' ',ap_mat),tel,direccion,mail,horas_trab,turno,antiguedad from empleado;
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_get_user_name` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_get_user_name`(usr_in nvarchar(50))
+begin
+
+declare id_usr_calc int(2);
+
+set id_usr_calc = (select id_empleado from login where usr = usr_in);
+select concat(nombre,' ',ap_pat,' ',ap_mat) from empleado where id_empleado = id_usr_calc;
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_login` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_login`(usr_in nvarchar(100),pwd_in nvarchar(100))
+begin
+
+declare resultado int(2);
+set resultado = (select count(*) from login where usr = usr_in and pwd = pwd_in);
+
+if resultado = 1 then
+select id_empleado, admin from login where usr=usr_in and pwd = pwd_in;
+end if;
+
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_modify_user` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_modify_user`(usr_in nvarchar(100),nombre_in nvarchar(100),ap_in nvarchar(50),am_in nvarchar(50),tel_in nvarchar(20),dir_in nvarchar(200),mail_in nvarchar(50),horas_in nvarchar(2),turno_in nvarchar(200),antiguedad_in int(15))
+begin
+
+declare id_usr_calc int(2);
+
+set id_usr_calc = (select id_empleado from login where usr = usr_in);
+
+UPDATE empleado set nombre = nombre_in, ap_pat = ap_in,ap_mat = am_in,tel = tel_in, direccion = dir_in, mail = mail_in, horas_trab = horas_in,turno = turno_in,antiguedad = antiguedad_in where id_empleado = id_usr_calc;
+
+
+select concat('Modificacion exitosa!') as id_getted;
+
+
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-13 22:43:52
